58452389de4c40c7f174bd1fac083f6ba30d4d53
abi <abimael.monarrez58@gmail.com>
2022-02-07 17:00:12 -0700

    finish the queries that display the tables

--- queries.js
+++ queries.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
 )
 
 module.exports = {
-    viewDep: () => db.promise().query("SELECT * FROM department"),
-    viewRoles: () => db.promise().query("SELECT * FROM role"),
-    viewEmp: () => db.promise().query("SELECT * FROM employee")
+    viewDep: () => db.promise().query("SELECT id, name AS department FROM department"),
+    viewRoles: () => db.promise().query("SELECT r.id, r.title, r.salary, d.name AS department FROM role AS r JOIN department AS d ON r.department_id = d.id"),
+    viewEmp: () => db.promise().query("SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, m.last_name AS manager FROM employee AS e LEFT JOIN employee AS m ON e.manager_id = m.id JOIN role AS r ON e.role_id = r.id JOIN department AS d ON r.department_id = d.id")
 };
